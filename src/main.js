8e6fe7881379089ad5fe00d9f8ebc194ef7be2c8
sulenchy <sulaimanabiodun172@gmail.com>
2024-08-02 15:26:35 +0100

    initial commit

---
+++ src/main.js
@@ -0,0 +1,11 @@
+import '/style.css'
+import javascriptLogo from '/javascript.svg'
+import viteLogo from '/vite.svg'
+
+document.querySelector('#app').innerHTML = `
+  <div>
+    <h1>Auction site</h1>
+  </div>
+`
+
+
